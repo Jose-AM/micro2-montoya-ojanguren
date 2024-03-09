149a9d3f5f693e680e2ea8202d76bba5503ada49
José Montoya <j.montoya@correo.unimet.edu.ve>
2024-03-08 22:52:30 -0400

    firebase

--- micro2MontoyaOjanguren/vite.config.js
+++ micro2MontoyaOjanguren/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'micro', // Aquí especifica la carpeta de construcción (puedes usar otro nombre si prefieres)
+  },
+});
