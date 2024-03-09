4806f6bf3ec34e98b6bcc7a28bbae7338cf0254a
José Montoya <j.montoya@correo.unimet.edu.ve>
2024-03-09 14:51:20 -0400

    landing iniciado

--- micro2MontoyaOjanguren/src/App.jsx
+++ micro2MontoyaOjanguren/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
-import './.css'
+import appFirebase from '../src/firebase'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
+const auth = getAuth(appFirebase)
+import './App.css'
+import Login from "./components/Login"
+import Home from "./components/Home"
+
+
+
 
 function App() {
+  const [user, setUser] = useState(null)
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+      setUser(usuarioFirebase)
+    } else {
+      setUser(null)
+    }
+  })
 
   return (
     <div>
-
+      {user ? <Home correoUsuario = {usuario.email}/> : <Login />}
     </div>
   )
 }
